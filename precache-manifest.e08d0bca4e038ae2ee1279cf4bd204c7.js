7e38f99ea1f2c24a32664df852e8fa52eecf1aef
Tolokoban <fabien.petitjean@epfl.ch>
2019-11-08 19:37:00 +0100

    Updates

---
+++ precache-manifest.e08d0bca4e038ae2ee1279cf4bd204c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c41463bac1ddbf2e54f797eec1a60fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b45f872cd1818add00d",
+    "url": "/static/css/main.fb8fb674.chunk.css"
+  },
+  {
+    "revision": "1fa872ca67d189b18fee",
+    "url": "/static/js/2.3d59ee08.chunk.js"
+  },
+  {
+    "revision": "7b45f872cd1818add00d",
+    "url": "/static/js/main.282753b7.chunk.js"
+  },
+  {
+    "revision": "82b7c478f30495c4cb82",
+    "url": "/static/js/runtime-main.b5ba109a.js"
+  }
+]);
